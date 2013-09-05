0f7e00e52eb0cd27701f6b11df45ee2387ee1e1f
Eduardo Neves <odraudelp@gmail.com>
2013-09-04 22:10:25 -0300

    Configurações do heroku OK. ATENÇÃO para upar no heroku deve-se descomentar o image_uploader para fog. e o arquivo ./config/initializers/carriewave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIRN2EX7GS7EC7UOQ',
-    :aws_secret_access_key  => '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',
-    :region                 => 'us-east-1'
-  }
-  config.fog_directory  = 'arheroku'
-  config.asset_host     = 'http://arheroku.s3-website-us-east-1.amazonaws.com'
-  config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
-end
+#CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#    :provider               => 'AWS',
+#    :aws_access_key_id      => 'AKIAIRN2EX7GS7EC7UOQ',
+#    :aws_secret_access_key  => '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',
+#    :region                 => 'us-east-1'
+#  }
+#  config.fog_directory  = 'arheroku'
+#  config.asset_host     = 'http://arheroku.s3-website-us-east-1.amazonaws.com'
+#  config.fog_public     = true
+#  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+#end
